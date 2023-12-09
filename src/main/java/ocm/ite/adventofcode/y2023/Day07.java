a746ffe59ed7961cb3cab4948feb0d1d3181be52
issamtellissi <tellissi.i@sfeir.com>
2023-12-09 16:08:48 +0100

    Day 6 to day 7

--- src/main/java/ocm/ite/adventofcode/y2023/Day07.java
+++ src/main/java/ocm/ite/adventofcode/y2023/Day07.java
@@ -2,11 +2,40 @@ package ocm.ite.adventofcode.y2023;
 
 import ocm.ite.adventofcode.AocUtils;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day07 {
+
+    private static final Map<Character, Integer> strengthP1 = Map.of(
+            'A', 14,
+            'K', 13,
+            'Q', 12,
+            'J', 1,
+            'T', 10,
+            '9', 9,
+            '8', 8
+    );
+
+    private static final Map<Character, Integer> strengthP2 = Map.of(
+            '7', 7,
+            '6', 6,
+            '5', 5,
+            '4', 4,
+            '3', 3,
+            '2', 2
+    );
+
+    // ugly but works
+    private static final Map<Character, Integer> strength = new HashMap<>() {{
+        putAll(strengthP1);
+        putAll(strengthP2);
+    }};
 
     public static void main(String[] args) {
         part1();
@@ -14,51 +43,117 @@ public class Day07 {
     }
 
     public static void part1() {
-        long result = 1;
-        var lines = AocUtils.readLines("/input/2023/day06.txt");
-
-        var times = Arrays.stream(lines.get(0).substring("Time:".length()).split(" "))
-                .filter(Predicate.not(String::isBlank))
-                .map(Long::parseLong)
-                .toList();
-
-        var distances = Arrays.stream(lines.get(1).substring("Distance:".length()).split(" "))
-                .filter(Predicate.not(String::isBlank))
-                .map(Long::parseLong)
-                .toList();
-        for (int i =0; i < times.size(); i++) {
-            var time = times.get(i);
-            var distance = distances.get(i);
-
-            var delta = time * time - 4 * distance;
-            var tMin = (int) Math.floor(((double) time - Math.sqrt(delta)) / 2);
-            var tMax = (int) Math.floor(((double) time + Math.sqrt(delta)) / 2);
-            var range = tMax - tMin;
-            result *= range;
-        }
-        System.out.println(result);
+        // todo
     }
 
     public static void part2() {
-        long result = 1;
-        var lines = AocUtils.readLines("/input/2023/day06.txt");
+        var hands = AocUtils.mapLines("/input/2023/day07.txt", Hand::of)
+                .stream()
+                .sorted(Comparator.comparing(Hand::getCategory)
+                        .thenComparing(Hand::getC1)
+                        .thenComparing(Hand::getC2)
+                        .thenComparing(Hand::getC3)
+                        .thenComparing(Hand::getC4)
+                        .thenComparing(Hand::getC5)
+                )
+                .toList();
 
-        var time = Long.valueOf(
-                Arrays.stream(lines.get(0).substring("Time:".length()).split(" "))
-                        .filter(Predicate.not(String::isBlank))
-                        .collect(Collectors.joining(""))
-        );
-
-        var distance = Long.parseLong(Arrays.stream(lines.get(1).substring("Distance:".length()).split(" "))
-                .filter(Predicate.not(String::isBlank))
-                .collect(Collectors.joining("")));
-
-        var delta = time * time - 4 * distance;
-        var tMin = (int) Math.floor(((double) time - Math.sqrt(delta)) / 2);
-        var tMax = (int) Math.floor(((double) time + Math.sqrt(delta)) / 2);
-        var range = tMax - tMin;
-        result *= range;
-        System.out.println(result);
+        var sum = 0;
+        for (int i = 0; i < hands.size(); i++) {
+            sum += (i + 1) * hands.get(i).bid;
+        }
+        System.out.println(sum);
     }
 
+    private static class Hand {
+        public Integer getCategory() {
+            return category;
+        }
+
+        private final String actualHand;
+
+        private final Map<Character, Integer> occ;
+        private final Integer bid;
+        private final Integer category;
+
+        private final int c1;
+        private final int c2;
+        private final int c3;
+        private final int c4;
+        private final int c5;
+
+        private Hand(String actualHand, Map<Character, Integer> occ, Integer bid, Integer category) {
+            this.actualHand = actualHand;
+            this.occ = occ;
+            this.bid = bid;
+            this.category = category;
+
+            c1 = strength.get(actualHand.charAt(0));
+            c2 = strength.get(actualHand.charAt(1));
+            c3 = strength.get(actualHand.charAt(2));
+            c4 = strength.get(actualHand.charAt(3));
+            c5 = strength.get(actualHand.charAt(4));
+        }
+
+        static Hand of(String hand) {
+            var input = hand.split(" ");
+            var handCards = input[0];
+            var handBid = input[1];
+            var occ = IntStream.range(0, handCards.length()).mapToObj(handCards::charAt)
+                    .collect(Collectors.groupingBy(Function.identity()))
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            e -> e.getValue().size()
+                    ));
+            return new Hand(handCards, occ, Integer.valueOf(handBid), calculateCategory(occ));
+        }
+
+        private static Integer calculateCategory(Map<Character, Integer> occ) {
+            int occJ = Optional.ofNullable(occ.get('J')).orElse(0);
+            var map2 = new HashMap<>(occ);
+            map2.remove('J');
+            int size = map2.size();
+            if (size <= 1) {
+                return 7;
+            }
+            if (size == 2) {
+                if (occ.containsValue(4 - occJ)) {
+                    return 6;
+                }
+                return 5;
+            }
+            if (size == 3) {
+                if (occ.containsValue(3 - occJ)) {
+                    return 4;
+                }
+                return 3;
+            }
+            if (size == 4) {
+                return 2;
+            }
+            return 1;
+        }
+
+        public int getC1() {
+            return c1;
+        }
+
+        public int getC2() {
+            return c2;
+        }
+
+        public int getC3() {
+            return c3;
+        }
+
+        public int getC4() {
+            return c4;
+        }
+
+        public int getC5() {
+            return c5;
+        }
+    }
 }
---
+++ src/main/java/ocm/ite/adventofcode/y2023/Day08.java
@@ -0,0 +1,178 @@
+package ocm.ite.adventofcode.y2023;
+
+import ocm.ite.adventofcode.AocUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Day08 {
+
+    public static void main(String[] args) {
+        part1();
+        part2();
+    }
+
+    public static void part1() {
+        var pattern = Pattern.compile("(\\w{3}) = \\((\\w{3}), (\\w{3})\\)");
+        Map<String, Node> network = new HashMap<>();
+        var linesIterator = AocUtils.readLines("/input/2023/day08.txt").iterator();
+
+        var instructions = linesIterator.next();
+        linesIterator.next();
+        String line;
+        while (linesIterator.hasNext()) {
+            line = linesIterator.next();
+            var matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                var currentNode = matcher.group(1);
+                var leftNode = matcher.group(2);
+                var rightNode = matcher.group(3);
+
+                var curr = network.computeIfAbsent(currentNode, Node::new);
+                curr.left = network.computeIfAbsent(leftNode, Node::new);
+                curr.right = network.computeIfAbsent(rightNode, Node::new);
+            }
+        }
+        long i = 0;
+        Node curr = network.get("AAA");
+
+        while (!curr.label.equals("ZZZ")) {
+            switch (instructions.charAt((int) (i % instructions.length()))) {
+                case 'L' -> curr = curr.left;
+                case 'R' -> curr = curr.right;
+            }
+            i++;
+        }
+
+        System.out.println(i);
+    }
+
+    public static void part2() {
+        var pattern = Pattern.compile("(\\w{3}) = \\((\\w{3}), (\\w{3})\\)");
+        Map<String, Node> network = new HashMap<>();
+        List<Node> starting = new ArrayList<>();
+        var linesIterator = AocUtils.readLines("/input/2023/day08.txt").iterator();
+
+        var instructions = linesIterator.next();
+        linesIterator.next();
+        String line;
+        while (linesIterator.hasNext()) {
+            line = linesIterator.next();
+            var matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                var currentNode = matcher.group(1);
+                var leftNode = matcher.group(2);
+                var rightNode = matcher.group(3);
+
+                var curr = network.computeIfAbsent(currentNode, Node::new);
+                curr.left = network.computeIfAbsent(leftNode, Node::new);
+                curr.right = network.computeIfAbsent(rightNode, Node::new);
+                if (curr.label.endsWith("A")) {
+                    starting.add(curr);
+                }
+                if (curr.label.endsWith("Z")) {
+                    curr.isFinal = true;
+                }
+            }
+        }
+        long i = 0;
+        Node[] curr = starting.toArray(new Node[starting.size()]);
+        long[] prevFinal = new long[starting.size()];
+        long[] cycleStart = new long[starting.size()];
+        long[] prevCycle = new long[starting.size()];
+        boolean[] hasCycle = new boolean[starting.size()];
+
+        boolean endReached = false;
+        boolean allHaveCycle = false;
+        while (!endReached && !allHaveCycle) {
+            endReached = true;
+            allHaveCycle = false;
+            switch (instructions.charAt((int) (i % instructions.length()))) {
+                case 'L' -> {
+                    for (int j = 0; j < curr.length; j++) {
+                        curr[j] = curr[j].left;
+                        endReached = endReached && curr[j].isFinal;
+                        if (curr[j].isFinal) {
+                            if (cycleStart[j] == 0) {
+                                cycleStart[j] = i;
+                            }
+                            if (prevFinal[j] != 0) {
+                                var cycle = i - prevFinal[j];
+                                if (prevCycle[j] == cycle) {
+                                    hasCycle[j] = true;
+                                    allHaveCycle = true;
+                                    for (boolean hc : hasCycle) {
+                                        allHaveCycle = allHaveCycle && hc;
+                                    }
+                                }
+                                prevCycle[j] = cycle;
+                            }
+                            prevFinal[j] = i;
+                        }
+                    }
+                }
+                case 'R' -> {
+                    for (int j = 0; j < curr.length; j++) {
+                        curr[j] = curr[j].right;
+                        endReached = endReached && curr[j].isFinal;
+                        if (curr[j].isFinal) {
+                            if (cycleStart[j] == 0) {
+                                cycleStart[j] = i;
+                            }
+                            if (prevFinal[j] != 0) {
+                                var cycle = i - prevFinal[j];
+                                if (prevCycle[j] == cycle) {
+                                    hasCycle[j] = true;
+                                    allHaveCycle = true;
+                                    for (boolean hc : hasCycle) {
+                                        allHaveCycle = allHaveCycle && hc;
+                                    }
+                                }
+                                prevCycle[j] = cycle;
+                            }
+                            prevFinal[j] = i;
+                        }
+                    }
+                }
+            }
+            i++;
+        }
+        var rs = 1L;
+        for (long c : prevCycle) {
+            rs = ppcm(rs, c);
+        }
+
+        System.out.println(rs);
+    }
+
+
+    public static long ppcm(long nb1, long nb2) {
+        long product, rest, ppcm;
+
+        product = nb1 * nb2;
+        rest = nb1 % nb2;
+        while (rest != 0) {
+            nb1 = nb2;
+            nb2 = rest;
+            rest = nb1 % nb2;
+        }
+        ppcm = product / nb2;
+        return ppcm;
+    }
+
+    private static class Node {
+        private final String label;
+
+        boolean isFinal;
+
+        private Node left;
+        private Node right;
+
+        private Node(String label) {
+            this.label = label;
+        }
+    }
+}
