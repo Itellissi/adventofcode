9c8c5bad31983b2e51f6101d91018be9d9f6bf1f
issamtellissi <tellissi.i@sfeir.com>
2024-12-03 06:24:21 +0100

    Day02

--- src/main/kotlin/com/ite/aoc/y2024/Day02.kt
+++ src/main/kotlin/com/ite/aoc/y2024/Day02.kt
@@ -2,20 +2,58 @@ package com.ite.aoc.y2024
 
 import com.ite.aoc.AocDay
 import ocm.ite.adventofcode.AocUtils
+import kotlin.math.abs
 
-class Day02 : AocDay<List<String>>(
+class Day02 : AocDay<List<List<Int>>>(
     day = 2,
     year = 2024,
 ) {
-    override fun part1(entries: List<String>): Any? {
-        return "Hello"
+
+    override fun part1(entries: List<List<Int>>): Any? = common(entries, 0)
+    override fun part2(entries: List<List<Int>>): Any? = common(entries, 1)
+
+    private fun common(entries: List<List<Int>>, maxDumpCount: Int): Int {
+        var count = 0
+        for (r in entries) {
+            var dumpCount = 0
+            var safe = true
+            var asCount = 0
+            var descCount = 0
+            // vote direction
+            for (i in 1..3) {
+                if (r[i] > r[i - 1]) asCount++
+                if (r[i] < r[i - 1]) descCount++
+            }
+            val asc = asCount > descCount
+            var i = 0
+            while (safe && i < r.size - 1) {
+                safe = isSafe(r, i, i + 1, asc)
+                if (!safe && dumpCount < maxDumpCount) {
+                    dumpCount++
+                    safe = i + 2 !in r.indices || isSafe(r, i, i + 2, asc)
+                    if (safe) {
+                        i++
+                    } else {
+                        safe = i - 1 !in r.indices || isSafe(r, i - 1, i + 1, asc)
+                    }
+                }
+                i++
+            }
+            if (safe) {
+                count++
+            }
+        }
+
+        return count
     }
 
-    override fun part2(entries: List<String>): Any? {
-        return "Aoc"
+    private fun isSafe(r: List<Int>, i: Int, j: Int, asc: Boolean): Boolean {
+        val diff = r[j] - r[i]
+        return abs(diff) in 1..3 && ((diff > 0 && asc) || diff < 0 && !asc)
     }
 
-    override fun convert(file: String): List<String> = AocUtils.mapLines(file) { _, l -> l }
+    override fun convert(file: String): List<List<Int>> =
+        AocUtils.mapLines(file) { _, l -> l.split(" ").map { it.toInt() } }
 
 }
 
